153d81a8c991cc60cf6085891dd7cc5ddbceae5a
John Perry <johnperry@dls.net>
2016-01-30 20:40:50 -0600

    Add IPUtil class

---
+++ source/java/IPUtil.java
@@ -0,0 +1,49 @@
+/*---------------------------------------------------------------
+*  Copyright 2015 by the Radiological Society of North America
+*
+*  This source software is released under the terms of the
+*  RSNA Public License (http://mirc.rsna.org/rsnapubliclicense.pdf)
+*----------------------------------------------------------------*/
+
+package org.rsna.httptest;
+
+import java.net.InetAddress;
+import java.net.Inet4Address;
+import java.net.NetworkInterface;
+import java.util.Enumeration;
+
+/**
+ * A class for getting the IP address of the host computer.
+ */
+public class IPUtil {
+
+	static final String def = "127.0.0.1";
+
+	/**
+	 * Get the IP address of the host computer, or the loopback address
+	 * (127.0.0.1) if the operation fails.
+	 * @return the IP Address string.
+	 */
+	public static String getIPAddress() {
+		try {
+			//Get all the network interfaces
+			Enumeration<NetworkInterface> nwEnum = NetworkInterface.getNetworkInterfaces();
+
+			//Return the first IPv4 address that is not a loopback address.
+			while (nwEnum.hasMoreElements()) {
+				NetworkInterface nw = nwEnum.nextElement();
+				Enumeration<InetAddress> ipEnum = nw.getInetAddresses();
+				while (ipEnum.hasMoreElements()) {
+					InetAddress ina = ipEnum.nextElement();
+					if ((ina instanceof Inet4Address) && !ina.isLoopbackAddress()) {
+						return ina.getHostAddress();
+					}
+				}
+			}
+		}
+		catch (Exception ignore) { }
+		return def;
+	}
+
+}
+
